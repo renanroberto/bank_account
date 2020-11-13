9072950a38936b40a28a71e2467f9d29ffb294ba
Renan <renanroberto1@gmail.com>
2020-11-13 13:42:51 -0300

    create code generator

---
+++ lib/bank_account/referral.ex
@@ -0,0 +1,20 @@
+defmodule BankAccount.Referral do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "referrals" do
+    field :code, :integer
+    field :client_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(referral, attrs) do
+    referral
+    |> cast(attrs, [:code, :client_id])
+    |> validate_required([:code, :client_id])
+    |> unique_constraint(:code)
+    |> unique_constraint(:client_id)
+  end
+end
---
+++ priv/repo/migrations/20201113163858_create_referrals.exs
@@ -0,0 +1,15 @@
+defmodule BankAccount.Repo.Migrations.CreateReferrals do
+  use Ecto.Migration
+
+  def change do
+    create table(:referrals) do
+      add :code, :integer, null: false
+      add :client_id, references(:clients, on_delete: :nothing), null: false
+
+      timestamps()
+    end
+
+    create unique_index(:referrals, [:code])
+    create unique_index(:referrals, [:client_id])
+  end
+end
